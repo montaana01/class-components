5448308dfb35ea86703ad8cdd21e8d2784013240
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-07-14 01:53:28 +0300

    style: prettier fixes

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,13 +1,13 @@
-import {Component, type ReactNode } from 'react';
+import { Component, type ReactNode } from 'react';
 import styles from './container.module.scss';
 
 type Props = {
   children: ReactNode;
-}
+};
 
 export default class Container extends Component<Props> {
   render() {
     const { children } = this.props;
     return <div className={styles.container}>{children}</div>;
   }
-};
+}
