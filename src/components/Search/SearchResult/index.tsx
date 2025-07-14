a5c92f0cb01549a3f307f634897118a000aba81c
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-07-14 04:53:21 +0300

    feat: complete search result box pt2

--- src/components/Search/SearchResult/index.tsx
+++ src/components/Search/SearchResult/index.tsx
@@ -10,7 +10,8 @@ interface Props {
 const SearchResult: React.FC<Props> = ({ items, isLoading, error }) => {
   if (isLoading) return <p className={'padding'}>Loading...</p>;
   if (error) return <p className="error">Error: {error}</p>;
-  if (items.length === 0) return <p className={'padding'}>Nothing was found!(</p>;
+  if (items.length === 0)
+    return <p className={'padding'}>Nothing was found!(</p>;
 
   return <CardList items={items} />;
 };
