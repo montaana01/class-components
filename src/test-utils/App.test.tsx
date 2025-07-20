ba24b9d56d50c72e1e32acb966c8c72ab3ae0f54
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-07-21 01:04:04 +0300

    test: add test case for App component

---
+++ src/test-utils/App.test.tsx
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('App', () => {
+  it('renders the main heading and includes SearchContainer and ErrorButton at App', () => {
+    render(<App />);
+    expect(screen.getByText(/class-component react app/i)).toBeInTheDocument();
+    expect(
+      screen.getByPlaceholderText(/type to search.../i)
+    ).toBeInTheDocument();
+    expect(screen.getByText(/set error/i)).toBeInTheDocument();
+  });
+});
