5edd702abd9ea4243fb7e21834e1dd8026012a4f
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-07-27 23:45:15 +0300

    refactor: upgrade card and container components to functional approach

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
 import Card from '../Card';
 
 type CardListProps = {
   items: { name: string; url: string }[];
 };
 
-export default class CardList extends React.Component<CardListProps> {
-  render() {
-    const { items } = this.props;
-    return (
-      <div className="cardList">
-        {items.map(({ name, url }) => (
-          <Card key={name} name={name} url={url} />
-        ))}
-      </div>
-    );
-  }
+export default function CardList({ items }: CardListProps) {
+  return (
+    <div className="cardList">
+      {items.map(({ name, url }) => (
+        <Card key={name} name={name} url={url} />
+      ))}
+    </div>
+  );
 }
