86d51b4eeb17f9b74f82db618321878f647728c4
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-07-27 22:39:40 +0300

    refactor: remove redundant annotation

--- src/api/pokeApi.ts
+++ src/api/pokeApi.ts
@@ -11,7 +11,7 @@ export async function fetchAbilities(
   limit: number,
   paginationUrl?: string
 ): Promise<PokeApiResponse> {
-  const url: string = paginationUrl ?? `${API_URL}/ability?limit=${limit}`;
+  const url = paginationUrl ?? `${API_URL}/ability?limit=${limit}`;
   const response = await fetch(url);
   if (!response.ok) throw new Error('Error while data loading!');
   return response.json();
