00e0a9e184a4a362571fcd379f728c39acd14924
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-07-28 02:05:53 +0300

    feat: fix App test error

--- src/test-utils/App.test.tsx
+++ src/test-utils/App.test.tsx
@@ -2,10 +2,15 @@ import '@testing-library/jest-dom';
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { MemoryRouter } from 'react-router';
 
 describe('App', () => {
   it('renders the main heading and includes SearchContainer and ErrorButton at App', () => {
-    render(<App />);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
     expect(screen.getByText(/class-component react app/i)).toBeInTheDocument();
     expect(
       screen.getByPlaceholderText(/type to search.../i)
