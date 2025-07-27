be5d6b90a2b861bd6568b6ed7955befe3129ddaa
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-07-27 23:22:46 +0300

    refactor: upgrade to functional approach

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-
 type CardProps = {
   name: string;
   url: string;
 };
 
-export default class Card extends React.Component<CardProps> {
-  render() {
-    const { name, url } = this.props;
-    return (
-      <a href={url}>
-        <strong>{name}</strong>
-      </a>
-    );
-  }
+export default function Card({ name, url }: CardProps) {
+  return (
+    <a href={url}>
+      <strong>{name}</strong>
+    </a>
+  );
 }
