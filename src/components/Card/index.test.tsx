3799f1587b0bf3fafaee393b7a44d26f6005568e
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-08-02 02:11:11 +0300

    refactor: update card components tests !

--- src/components/Card/index.test.tsx
+++ src/components/Card/index.test.tsx
@@ -6,15 +6,6 @@ import SearchResult from '../Search/SearchResult';
 import { mockData } from '../../test-utils/mock-constants.ts';
 
 describe('Card Component', () => {
-  const baseUrl = 'https://example.com';
-  it('renders an anchor with the correct href and name inside component', () => {
-    render(<Card options={mockData.results[3]} />);
-    const linkElement = screen.getByRole('link');
-    expect(linkElement).toBeInTheDocument();
-    expect(linkElement).toHaveAttribute('href', baseUrl);
-    expect(linkElement).toHaveTextContent('Example');
-  });
-
   it('does not render anchor when url is missing', () => {
     render(<Card options={{ ...mockData.results[3], url: '' }} />);
     const linkElement = screen.queryByRole('link');
