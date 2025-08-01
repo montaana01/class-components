c99870dff21c8ff864bfde2073f04b51f6e13a71
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-08-01 21:50:17 +0300

    feat: update logic of creating request to API

--- src/api/apiDriver.ts
+++ src/api/apiDriver.ts
@@ -1,22 +1,15 @@
 import { API_PATH, API_URL, type ApiResponse } from './constants';
 
 export type FetchApiOptions = {
-  paginationUrl?: string;
+  page?: string;
   name?: string;
 };
 
 export async function fetchApi<T>(
   options: FetchApiOptions = {}
 ): Promise<ApiResponse<T>> {
-  const { paginationUrl, name } = options;
-
-  let url: string;
-  if (paginationUrl) {
-    url = paginationUrl;
-  } else {
-    const baseUrl = `${API_URL}/${API_PATH}`;
-    url = name ? `${baseUrl}/?name=${String(name).trim()}` : baseUrl;
-  }
+  const { page, name } = options;
+  const url = `${API_URL}/${API_PATH}/?${page ? 'page=' + page : ''}${name ? '&name=' + name : ''}`;
   const response = await fetch(url);
   if (!response.ok) throw new Error('Error while data loading!');
   return response.json();
