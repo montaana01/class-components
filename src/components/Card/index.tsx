db451dbd11f0d4fe7b6a664f90bbce604c429d91
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-08-03 19:35:59 +0300

    feat: implement checkbox on product card with some style updates

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -13,21 +13,27 @@ export default function Card({ options }: CardProps) {
       acc[params[0]] = params[1];
       return acc;
     }, {});
+
   const handleCardClick = () => {
     queryParams.active = options.name;
     navigate(
       `/search?${queryParams.page ? 'page=' + queryParams.page : ''}${queryParams.query ? '&query=' + queryParams.query : ''}${queryParams.active ? '&active=' + queryParams.active : ''}`
     );
   };
+
+  const handleCheckboxClick = () => {
+    return null;
+  };
+
   return (
-    <div className={'card'} onClick={handleCardClick}>
+    <div className={'card'}>
       <div
         className={'card-shadow'}
         style={{
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${options.image})`,
         }}
       />
-      <div className={'card-container'}>
+      <div className={'card-container'} onClick={handleCardClick}>
         <h2>{options.name}</h2>
         <div>
           <p>
@@ -42,6 +48,14 @@ export default function Card({ options }: CardProps) {
           </p>
           <p>🎬 Appears in {options.episode.length} episodes</p>
         </div>
+      </div>
+      <div className={'card-checkbox'}>
+        <label htmlFor={options.name}>Add this Item!</label>
+        <input
+          id={options.name}
+          type={'checkbox'}
+          onChange={handleCheckboxClick}
+        />
       </div>
     </div>
   );
