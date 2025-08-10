7c987a3b017992a198cb9d3c57268cd08d735615
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-08-10 05:51:14 +0300

    feat: set caching react-queries!

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,14 @@ import MainPage from './components/features/MainPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
