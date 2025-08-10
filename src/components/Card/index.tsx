acc74a54cfd26bcc0399fa45d7ead1af8a49ebc0
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-08-11 02:28:47 +0300

    feat: use parseQuery helper

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,6 +2,7 @@ import './card.scss';
 import { useLocation, useNavigate } from 'react-router';
 import type { CardProps, CharacterDetail, QueryParams } from '../../types';
 import { useSelectedItemsStore } from '../../store/selectedItemsStore.ts';
+import { parseQueryParams } from '../../helpers/parseQueryParams';
 
 const inputTitle = {
   addItem: 'Add this Item!',
@@ -11,20 +12,15 @@ const inputTitle = {
 export default function Card({ cardOptions }: CardProps) {
   const navigate = useNavigate();
   const query = useLocation();
-  const queryParams: QueryParams = query.search
-    .split('?')[1]
-    .split('&')
-    .reduce((acc, el) => {
-      const params = el.split('=');
-      acc[params[0]] = params[1];
-      return acc;
-    }, {});
+  const queryParams: QueryParams = parseQueryParams(query.search);
   const { selectedItems, toggleItem } = useSelectedItemsStore();
   const isSelected = selectedItems.some(
     (card: CharacterDetail) => card.id === cardOptions.id
   );
 
   const handleCardClick = () => {
+    const params = new URLSearchParams(query.search);
+    params.set('active', cardOptions.id.toString());
     queryParams.active = cardOptions.id;
     navigate(
       `/search?${queryParams.page ? 'page=' + queryParams.page : ''}${queryParams.query ? '&query=' + queryParams.query : ''}${queryParams.active ? '&active=' + queryParams.active : ''}`
