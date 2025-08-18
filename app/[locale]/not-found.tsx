f6170dbbd2e41c5bde7e82c556c7b70484b583ca
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-08-18 06:41:57 +0300

    feat: implement custom 404 error

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,14 +1,2 @@
-import { useTranslations } from 'next-intl';
-import Container from '@/components/Container';
-import Link from 'next/link';
-
-export default function NotFound() {
-  const t = useTranslations('NotFound');
-
-  return (
-    <Container>
-      <h1>{t('title')}</h1>
-      <Link href="/public">{t('buttonTitle')}</Link>
-    </Container>
-  );
-}
+import NotFound from '@/components/NotFound';
+export default NotFound;
