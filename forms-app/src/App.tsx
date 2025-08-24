c8ee0f12194ba1960e2fd92df09a03426b094e29
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-08-24 03:03:16 +0300

    feat: update project markup with adding new component

--- forms-app/src/App.tsx
+++ forms-app/src/App.tsx
@@ -1,24 +1,36 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { Container } from '@/components/Container';
+import '@/assets/styles/global.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="header">
+        <Container>
+          <div className="header_wrapper">
+            <h1>Vite + React</h1>
+          </div>
+        </Container>
+      </header>
+      <main>
+        <Container>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+        </Container>
+      </main>
+      <footer>
+        <Container>
+          <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        </Container>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- forms-app/src/main.tsx
+++ forms-app/src/main.tsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement: HTMLDivElement = document.createElement('div');
+rootElement.id = 'root';
+document.body.append(rootElement);
+
+createRoot(rootElement).render(<App />);
