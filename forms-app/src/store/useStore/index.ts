33e9cc674dc0d50f13fcc93236a85bec020dc5db
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-08-24 12:26:03 +0300

    refactor: fix style

--- forms-app/src/store/useStore/index.ts
+++ forms-app/src/store/useStore/index.ts
@@ -1,11 +1,12 @@
-import { create } from "zustand/react";
-import type { State } from "@/types";
+import { create } from 'zustand/react';
+import type { State } from '@/types';
 
 export const useStore = create<State>((set) => ({
   entries: [],
   countries: [],
-  addEntry: (entry) => set((state) => ({
-    entries: [{ ...entry, id: crypto.randomUUID(), createdAt: Date.now() }, ...state.entries]
-  })),
-  setCountries: (countries) => set({ countries: countries })
-}))
+  addEntry: (entry) =>
+    set((state) => ({
+      entries: [{ ...entry, id: crypto.randomUUID(), createdAt: Date.now() }, ...state.entries],
+    })),
+  setCountries: (countries) => set({ countries: countries }),
+}));
--- forms-app/src/types/index.ts
+++ forms-app/src/types/index.ts
@@ -1,19 +1,19 @@
 export type FormEntry = {
-  id: string
-  source: 'uncontrolled' | 'hookform'
-  name: string
-  age: number
-  email: string
-  gender: string
-  country: string
-  acceptTos: boolean
-  pictureBase64?: string | null
-  createdAt: number
-}
+  id: string;
+  source: 'uncontrolled' | 'hookform';
+  name: string;
+  age: number;
+  email: string;
+  gender: string;
+  country: string;
+  acceptTos: boolean;
+  pictureBase64?: string | null;
+  createdAt: number;
+};
 
 export type State = {
-  entries: FormEntry[]
-  countries: string[]
-  addEntry: (entry: Omit<FormEntry,'id'|'createdAt'>) => void
-  setCountries: (countries: string[]) => void
-}
+  entries: FormEntry[];
+  countries: string[];
+  addEntry: (entry: Omit<FormEntry, 'id' | 'createdAt'>) => void;
+  setCountries: (countries: string[]) => void;
+};
